a05cf26fb63e60f2c9f86bb5edcd84c1bfbc642a
William Antony <william@pathadan.com>
2018-02-22 12:48:54 -0600

    Initial Commit - ReactJS Boilerplate

---
+++ frontend/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+ReactDOM.render(
+  <div className="App">
+
+  </div>,
+  document.getElementById('root')
+);
+
+// import registerServiceWorker from './registerServiceWorker';
+// registerServiceWorker();
